d82103e0dfde8911d260ca44c7877ba92fd6a2a6
Orangeyness <reuben_bell@hotmail.com>
2016-05-20 17:11:48 +1200

    Add basic mouse events

--- WpfSharpDxControl/HwndWrapper.cs
+++ WpfSharpDxControl/HwndWrapper.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using SharpDX;
@@ -62,11 +64,51 @@ namespace WpfSharpDxControl
 
 			base.OnRenderSizeChanged(sizeInfo);
 		}
+
+		protected override IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			switch(msg)
+			{
+				case NativeMethods.WM_LBUTTONDOWN:
+					RaiseMouseEvent(MouseButton.Left, Mouse.MouseDownEvent);
+					break;
+
+				case NativeMethods.WM_LBUTTONUP:
+					RaiseMouseEvent(MouseButton.Left, Mouse.MouseUpEvent);
+					break;
+
+				case NativeMethods.WM_RBUTTONDOWN:
+					RaiseMouseEvent(MouseButton.Right, Mouse.MouseDownEvent);
+					break;
+
+				case NativeMethods.WM_RBUTTONUP:
+					RaiseMouseEvent(MouseButton.Right, Mouse.MouseUpEvent);
+					break;
+			}
+
+			return base.WndProc(hwnd, msg, wParam, lParam, ref handled);
+		}
+
+		private void RaiseMouseEvent(MouseButton button, RoutedEvent @event)
+        {
+			RaiseEvent(new MouseButtonEventArgs(Mouse.PrimaryDevice, 0, button)
+			{
+				RoutedEvent = @event,
+				Source = this,
+			});
+		}
 	}
 
 	internal class NativeMethods
 	{
+		// ReSharper disable InconsistentNaming
 		public const int WS_CHILD = 0x40000000;
 		public const int WS_VISIBLE = 0x10000000;
+
+		public const int WM_LBUTTONDOWN = 0x0201;
+		public const int WM_LBUTTONUP = 0x0202;
+		public const int WM_RBUTTONDOWN = 0x0204;
+		public const int WM_RBUTTONUP = 0x0205;
+		// ReSharper restore InconsistentNaming
 	}
 }
